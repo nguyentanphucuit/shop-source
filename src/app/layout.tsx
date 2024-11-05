8419909390802c589facae4f3e02c5e0587993ab
Peter Nguyen <peter.nguyen@siliconstack.com.au>
2024-11-05 23:48:35 +0700

    remove header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <Header /> */}
       <body className={inter.className}>
         <Navbar />
         {children}
